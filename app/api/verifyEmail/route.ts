0810df9334cdd293e2a2306de27d7e1def02c638
Leonardo Lima <leonardostlima@gmail.com>
2023-08-09 23:01:51 -0300

    fix: next auth

---
+++ app/api/verifyEmail/route.ts
@@ -0,0 +1,43 @@
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function POST(request: NextRequest) {
+  try {
+    prisma.$connect;
+    const body = await request.json();
+    const token = body.token;
+    const userToken = await prisma.verificationToken.findUnique({
+      where: {
+        token: token,
+      },
+    });
+
+    if (!userToken) {
+      return new NextResponse("Invalid token!", { status: 400 });
+    }
+    console.log(userToken);
+    await prisma.user.update({
+      where: {
+        id: userToken.identifier,
+      },
+      data: {
+        emailVerified: new Date(),
+      },
+    });
+    await prisma.verificationToken.update({
+      where: {
+        token: token,
+      },
+      data: {
+        token: undefined,
+        expires: undefined,
+      },
+    });
+    prisma.user.upsert;
+    return new NextResponse(`Password update successfully!`);
+  } catch (error: any) {
+    return new NextResponse(`Error: ${error}`, { status: 500 });
+  }
+}
